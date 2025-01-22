d4916b56be6e8f9b7ca596c7af04fe107b7d0f1f
Voichita Maria <sabo_voichita@yahoo.com>
2025-01-22 21:21:56 +0100

    add button for changing colors on click events

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello, World!");
+const button = document.getElementById("button");
+
+const colors = [
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "rebeccapurple",
+  "violet",
+];
+function change() {
+  document.body.style.background = colors[Math.floor(7 * Math.random())];
+}
+
+button.addEventListener("click", change);
+
+// console.log("Hello, World!", button, colors);
